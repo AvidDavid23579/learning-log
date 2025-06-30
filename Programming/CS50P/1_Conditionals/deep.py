70576ea617e08289405aafe7592c88eecec2add5
AvidDavid23579 <aviddavid23579@gmail.com>
2025-06-30 12:38:24 +0700

    Delete Programming/CS50P/1_Conditionals directory

--- Programming/CS50P/1_Conditionals/deep.py
+++
@@ -1,7 +0,0 @@
-ans = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
-if ans.strip() == "42":
-    print("Yes")
-elif ans.lower().replace("-", " ").strip() == "forty two":
-    print("Yes")
-else:
-    print("No")
