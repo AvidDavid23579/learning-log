3db271cdd0c49ff6a831cf8a9380b22ab6500aa8
AvidDavid23579 <aviddavid23579@gmail.com>
2025-07-01 17:14:39 +0700

    Update P9_Special_Pythagorean_Triplet.py

--- Programming/Project-Euler/Code_Files/P9_Special_Pythagorean_Triplet.py
+++ Programming/Project-Euler/Code_Files/P9_Special_Pythagorean_Triplet.py
@@ -1,6 +1,6 @@
-# a = m**2 + n**2
+# a = m**2 - n**2
 # b = 2mn
-# c = m**2 - n**2
+# c = m**2 + n**2
 # a + b + c = 2m**2 + 2mn
 # (a + b + c) / 2 = m * (m + n) = 500
 d1 = []
@@ -17,7 +17,7 @@ for i in range(len(d1)):
     a = m**2 - n**2
     b = 2*m*n
     c = m**2 + n**2
-    if a**2 + b**2 == c**2 and a > 0 and b > 0 and c > 0:
+    if a > 0 and b > 0 and c > 0:
         print(int(a*b*c))
         break
 
