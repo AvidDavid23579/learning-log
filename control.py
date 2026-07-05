05895ac8fd55b1f2c80727367843cea395fa7cd9
AvidDavid23579 <aviddavid23579@gmail.com>
2026-07-05 23:11:45 +0700

    Wrapped arm feedforward

--- control.py
+++ control.py
@@ -245,3 +245,19 @@ class TrapezoidalProfile:
 
         else:
             return 0
+
+class ArmFeedforward:
+    def __init__(self, mass, length, inertia, damping, gravity=9.81):
+        self.m = mass
+        self.l = length
+        self.I = inertia
+        self.b = damping
+        self.g = gravity
+
+    def calculate(self, theta_ref, omega_ref, alpha_ref):
+
+        gravity = self.m * self.g * (self.l / 2) * np.cos(theta_ref)
+        friction = self.b * omega_ref
+        inertia = self.I * alpha_ref
+
+        return gravity + friction + inertia
